282de19da9c83deee127c5e3c66170425030e3f0
Software Shinobi <troy@softwareshinobi.com>
2025-06-11 22:14:20 -0400

    updated compose things

--- provision.bash
+++ provision.bash
@@ -2,40 +2,24 @@
 
 ##
 
-#set -e;
+reset
 
-#set -x;
-
-## updating apt
-
-sudo apt-get update;
+clear
 
 ##
 
-sudo apt -y install ca-certificates curl;
+set -e
 
-### APT package dependency setup
+set -x
 
-sudo install -m 0755 -d /etc/apt/keyrings
+##
 
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+cd /tmp/
 
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+git clone https://github.com/softwareshinobi/provision-aws-ec2-ubuntu-docker.git install-docker
 
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  tee /etc/apt/sources.list.d/docker.list > /dev/null
+cd install-docker
 
-## docker engine install
+sudo bash provision.bash
 
-sudo apt update
-
-sudo apt purge -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose docker docker.io
-
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-sudo docker run hello-world
-
-echo "finished provision..."
-
+echo "finished installing docker"
